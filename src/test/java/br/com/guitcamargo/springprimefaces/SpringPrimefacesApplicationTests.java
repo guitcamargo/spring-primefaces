e99d087cc197b2da092999f7b947931656e2b187
Guilherme Theodoro Camargo <gtheodorocamargo@gmail.com>
2023-03-27 22:00:02 -0300

    Primeiro commit

---
+++ src/test/java/br/com/guitcamargo/springprimefaces/SpringPrimefacesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guitcamargo.springprimefaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrimefacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
